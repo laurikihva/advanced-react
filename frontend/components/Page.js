fa912abb256517e8cf00b90c4cb67c403998cd86
Lauri <lauri.kihva@telia.ee>
2019-03-11 20:04:39 +0200

    added global theme under page component, added styles to header component

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React from 'react';
+import styled, {ThemeProvider, injectGlobal} from 'styled-components';
 
 import Header from './Header';
 import Meta from './Meta';
 
+const theme = {
+    red: '#FF0000',
+    black: '#393939',
+    grey: '#3A3A3A',
+    lightGrey: '#E1E1E1',
+    offWhite: '#EDEDED',
+    maxWidth: '1000px',
+    boxShadow: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StyledPage = styled.div`
+    background: white;
+    color: ${props => props.theme.black};
+`;
+
+const Container = styled.div`
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+    padding: 2rem;
+`;
+
 export default class Page extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <Meta />
-                <Header />
-                {this.props.children}
-            </React.Fragment>
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta />
+                    <Header />
+                    <Container>
+                        {this.props.children}
+                    </Container>
+                </StyledPage>
+            </ThemeProvider>
         )
-    } 
+    }
 }
