641107fa271a9c224677456d628830ea4cb87ff9
Lauri <lauri.kihva@telia.ee>
2019-04-09 17:41:13 +0300

    password reset flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
@@ -99,7 +101,53 @@ const Mutations = {
     signout(parent, args, ctx, info) {
         ctx.response.clearCookie('token');
 
-        return  { message: 'Successfully logged out!' }
+        return { message: 'Successfully logged out!' };
+    },
+
+    async requestReset(parent, args, ctx, info) {
+        // 1. Check if this is real user
+        const user = await ctx.db.query.user({ where: { email: args.email }});
+
+        if (!user) {
+            throw new Error(`No such user found for email ${args.email}`);
+        }
+
+        // 2. Set a reset token and expirty on that user
+        const randomBytesPromisified = promisify(randomBytes);
+        const resetToken = (await randomBytesPromisified(20)).toString('hex');
+        const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
+        const res = await ctx.db.mutation.updateUser({
+            where: { email: args.email },
+            data: { resetToken, resetTokenExpiry },
+        });
+        return { message: 'Thanks!' };
+        // 3. Email them that reset token
+    },
+
+    async resetPassword(parent, args, ctx, info) {
+        // 1. Check if the passwords matxh
+        if (args.password !== args.confirmPassword) {
+            throw new Error('Your Password don\'t match!');
+        }
+
+        // 2. Check if its a legit reset token
+        // 3. Check if its expired
+        const [user] = ctx.db.query.users({
+            where: {
+                resetToken: args.resetToken,
+                resetTokenExpiry_gte: Date.now() - 3600000
+            }
+        });
+
+        if (!user) {
+            throw new Error('This token is either invalid or expired!');
+        }
+        // 4. Hash their new password
+        // 5. Save their new password to their user and remove old resetToken fields
+        // 6. Generate JWT
+        // 7. Set the JWT cookie
+        // 8. Return the new user
+        // 9. 
     }
 };
 
