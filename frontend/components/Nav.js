262cedc3bedd6e51518c411539d44cbbb2cfb723
Lauri <lauri.kihva@telia.ee>
2019-04-19 20:17:51 +0300

    animated cart count change

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { Mutation } from 'react-apollo';
 
+import CartCount from './CartCount';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
@@ -27,7 +28,10 @@ const Nav = () => (
                         <Signout />
                         <Mutation mutation={TOGGLE_CART_MUTATION}>
                             {(toggleCart) => (
-                                <button onClick={toggleCart}>My Cart</button>
+                                <button onClick={toggleCart}>
+                                    My Cart
+                                    <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)} />
+                                </button>
                             )}
                         </Mutation>
                     </>
