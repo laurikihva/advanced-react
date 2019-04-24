c36fd759977fff1c5748e38fc509b3c746aa044b
Lauri <lauri.kihva@telia.ee>
2019-04-24 22:40:44 +0300

    displaying single order

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -30,7 +30,8 @@ function totalItems(cart) {
 
 export default class TakeMyMoney extends React.Component {
     onToken = async (res, createOrder) => {
-        console.log(res.id);
+        NProgress.start();
+
         // manually call the mutation once we have the stripe token
         const order = await createOrder({
             variables: {
@@ -39,8 +40,10 @@ export default class TakeMyMoney extends React.Component {
         }).catch(err => {
             alert(err.message);
         });
-
-        console.log(order);
+        Router.push({
+            pathname: '/order',
+            query: { id: order.data.createOrder.id }
+        });
     }
 
     render() {
