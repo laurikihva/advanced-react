3311be91956cef70185a44f4a55672417f8b6201
Lauri <lauri.kihva@telia.ee>
2019-04-24 23:24:13 +0300

    orders page made

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -50,6 +50,20 @@ const Query = {
 
         // 4. Return the order
         return order;
+    },
+
+    orders(parent, args, ctx, info) {
+        const { userId } = ctx.request;
+
+        if (!userId) {
+            throw new Error('You must be signed in!');
+        }
+
+        return ctx.db.query.orders({
+            where: {
+                user: { id: userId },
+            },
+        }, info);
     }
 };
 
