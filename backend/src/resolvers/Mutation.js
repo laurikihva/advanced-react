e65da7fd3bf5af8cedc979cca7b6802274dd6bf5
Lauri <lauri.kihva@telia.ee>
2019-04-24 18:31:38 +0300

    saving orders to database

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -312,6 +312,7 @@ const Mutations = {
                     id
                     description
                     image
+                    largeImage
                 }
             }
         }`);
@@ -325,10 +326,41 @@ const Mutations = {
             currency: 'EUR',
             source: args.token,
         });
+
         // 4. Convert the CartItems to OrderItems
+        const orderItems = user.cart.map(cartItem => {
+            const orderItem = {
+                ...cartItem.item,
+                quantity: cartItem.quantity,
+                user: { connect: { id: userId }},
+
+            };
+            delete orderItem.id;
+
+            return orderItem;
+        });
+
         // 5. Create the Order
+        const order = await ctx.db.mutation.createOrder({
+            data: {
+                total: charge.amount,
+                charge: charge.id,
+                items: { create: orderItems },
+                user: { connect: { id: userId }},
+            }
+        });
+
         // 6. Clean up - clear the users cart, delete cartItems
+        const cartItemsIds = user.cart.map(cartItem => cartItem.id);
+
+        await ctx.db.mutation.deleteManyCartItems({ 
+            where: {
+                id_in: cartItemsIds,
+            }
+        });
+
         // 7. Return the Order to the client.
+        return order;
     }
 };
 
--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -29,16 +29,18 @@ function totalItems(cart) {
 }
 
 export default class TakeMyMoney extends React.Component {
-    onToken = (res, createOrder) => {
+    onToken = async (res, createOrder) => {
         console.log(res.id);
         // manually call the mutation once we have the stripe token
-        createOrder({
+        const order = await createOrder({
             variables: {
                 token: res.id,
             }
         }).catch(err => {
             alert(err.message);
-        })
+        });
+
+        console.log(order);
     }
 
     render() {
@@ -52,7 +54,7 @@ export default class TakeMyMoney extends React.Component {
                                 currency="EUR"
                                 name="Sick Fits"
                                 description={`Order of ${totalItems(me.cart)} items!`}
-                                image={me.cart[0].item && me.cart[0].item.image}
+                                image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
                                 stripeKey="pk_test_LiP6BtByTjQ9lxAm0Z4ylRUP00WAQRyFNF"
                                 email={me.email}
                                 token={res => this.onToken(res, createOrder)}
